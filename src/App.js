8bd6adfccc0c27fa6d40324bfbbd998edbaf4026
Kahlan_Guha <kahlan.guha@gmail.com>
2025-05-04 15:22:07 +0530

    Initialization of the Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import Mainbody from "./components/Mainbody";
+import { ToastContainer} from 'react-toastify';
+
+
+const App = () => {
+
+
+    return (
+        <>
+        <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        />
+
+ 
+        
+        {/* <Mainbody/> */}
+        </>
+    );
+};
+
+export default App;

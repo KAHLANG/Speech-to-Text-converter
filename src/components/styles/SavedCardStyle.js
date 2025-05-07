5fbd35df33a7bf403a25a5cadddc1ce7288dd10c
Kahlan_Guha <kahlan.guha@gmail.com>
2025-05-07 23:42:20 +0530

    SavedCard.js and SavedCardStyle.js added

---
+++ src/components/styles/SavedCardStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const SavedCardWrapper = styled.section `
+        max-width: 1400px;
+        margin: 40px auto 40px auto;
+        
+        padding: 0 20px 0 20px;
+        margin-top: 40px;
+        display: flex;
+        justify-content: center;
+        gap: 50px;
+        flex-wrap: wrap;
+`
